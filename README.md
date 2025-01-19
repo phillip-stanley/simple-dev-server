638c18431aed0994aef165fed3148f92ab68222d
phillip stanley <phillip.stanley39@gmail.com>
2025-01-19 12:31:04 +0000

    prepare for npm package

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # simple-dev-server
-simple nodejs dev-server
+
+Simple-dev-server is a small node server that serves HTML files and implements
+hot-reloaded by injecting a script into each html file is serves. Connecting the page
+and the browser to the dev websocket server.
+
+## Installation
+
+install the package globally on your system
+```js
+npm i -g @phill-stanley/simple-dev-server
+```
+
+install as a dev dependency to your project
+```js
+npm i --save-dev @phill-stanley/simple-dev-server
+```
+
+## Configuration
+
+run with defaults. Add the following to your package.json scripts
+```js
+"dev-server": "simple-dev-server"
+```
+
+run custom setup. Add the following to your package.json scripts
+```js
+"dev-server": "WATCH_DIR='./src' HTTP_PORT=3000 HTTP_HOST='127.0.0.1' simple-dev-server"
+```
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+#!/usr/bin/env node
+
 const SocketServer = require('./src/socket-server/socket-server');
 const HttpServer = require('./src//http-server/http-server');
 const FileWatcher = require("./src/file-watcher/file-watcher");
 
+// environment variables
+const watchDirectory = process.env.WATCH_DIR || './public';
+const httpHost = process.env.HTTP_HOST || 'localhost';
+const httpPort = process.env.HTTP_PORT || 8000;
+
 // setup socket server
-const socketServer = new SocketServer()
-socketServer.setupServer(process.env.WS_PORT)
+const socketServer = new SocketServer();
+socketServer.setupServer(process.env.WS_PORT);
 
 // setup http server
-const httpServer = new HttpServer()
-httpServer.createServer(process.env.HTTP_HOST, process.env.HTTP_PORT)
+const httpServer = new HttpServer(watchDirectory);
+httpServer.createServer(httpHost, httpPort);
 
 // setup file watcher and subscribe to `fileChange` event
-const fileWatcher = new FileWatcher()
-fileWatcher.setupWatcher(process.env.WATCH_DIR)
+const fileWatcher = new FileWatcher();
+fileWatcher.setupWatcher(watchDirectory);
 fileWatcher.on('fileChanged', (filename) => {
     socketServer.broadcastMessage(`${filename} changed`)
 })
